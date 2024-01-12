ca2415cd5b46e2b7c1214a177f86bc652fcbec3a
SignorinoY <meetziyang@gmail.com>
2024-01-12 15:41:29 +0800

    :zap: update survtrans_control function parameters

--- R/survtrans_control.R
+++ R/survtrans_control.R
@@ -1,10 +1,10 @@
 #' Ancillary arguments for controlling survtrans fitting
 #' @param eps the convergence criteria for the CCD algorithm. Default is 1e-6.
 #' @param maxit the maximum number of iterations for the CCD algorithm.
-#'   Default is 1000.
+#'   Default is 500.
 #' @param patience the number of iterations without improvement before
-#'  stopping the CCD algorithm. Default is 3.
-survtrans_control <- function(eps = 1e-6, maxit = 1000, patience = 50) {
+#'  stopping the CCD algorithm. Default is 10.
+survtrans_control <- function(eps = 1e-6, maxit = 500, patience = 10) {
   if (eps <= 0) stop("Invalid convergence criteria")
   if (maxit < 0) stop("Invalid value for iterations")
   if (patience < 0) stop("Invalid value for patience")

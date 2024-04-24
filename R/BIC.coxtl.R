dee21b065a79cfed2e3a817c0feb5267c6a1ff24
SignorinoY <meetziyang@gmail.com>
2024-04-24 15:34:46 +0800

    :zap: add modified BIC for coxtl

--- R/BIC.coxtl.R
+++ R/BIC.coxtl.R
@@ -4,17 +4,22 @@
 #' should be the same as the data used to fit the model.
 #' @param group a factor specifying the group of each sample, which
 #' should be the same as the group used to fit the model.
+#' @param type a character string specifying the type of BIC to compute.
+#' The default is "trad", corresponding to C_{n}=1. The other option is
+#' "mod", corresponding to C_{n}=log(log(d)).
 #' @param ... Unused.
 #' @return the BIC of the model.
 #' @export
 BIC.coxtl <- function( # nolint: object_name_linter.
-    object, data, group, ...) {
-  loglik <- logLik(object, data, group)
-  eta <- object$eta
+    object, data, group, type = c("trad", "mod"), ...) {
+  type <- match.arg(type)
 
+  eta <- object$eta
   n_samples <- nrow(data)
   n_features <- nrow(eta)
+  n_groups <- ncol(eta)
 
+  loglik <- logLik(object, data, group)
   n_parameters <- n_features
   for (j in 1:n_features) {
     nonzeros <- eta[j, eta[j, ] != 0]
@@ -22,6 +27,6 @@ BIC.coxtl <- function( # nolint: object_name_linter.
       n_parameters <- n_parameters + length(unique(nonzeros))
     }
   }
-
+  c <- ifelse(type == "trad", 1, log(log(n_features * n_groups)))
   return(-2 * loglik + n_parameters * log(n_samples))
 }

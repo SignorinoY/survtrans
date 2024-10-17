c3e0318e5f66f35d265f0794dd6f39ee8aadca86
SignorinoY <meetziyang@gmail.com>
2024-10-17 18:56:06 +0800

    :bug: fixing bug in stopping criteria

---
+++ R/penalty.R
@@ -0,0 +1,17 @@
+penalty <- function(params, penalty, lambda, gamma) {
+  params.abs <- abs(params)
+  if (penalty == "lasso") {
+    grads <- ifelse(params.abs <= lambda, lambda, 0)
+  } else if (penalty == "SCAD") {
+    grads <- ifelse(
+      params.abs <= lambda,
+      lambda,
+      ifelse(
+        params.abs <= gamma * lambda,
+        (gamma * lambda - params) / (gamma - 1),
+        0
+      )
+    )
+  }
+  return(grads)
+}
--- R/survtrans_control.R
+++ R/survtrans_control.R
@@ -7,18 +7,26 @@
 #'  algorithm. Default is 1e-3.
 #' @param inner.maxit the maximum number of iterations for the inner loop
 #'  of the CCD algorithm. Default is 100.
+#' @param abstol the absolute tolerance for the proposed algorithm.
+#' Default is 1e-4.
+#' @param reltol the relative tolerance for the proposed algorithm.
+#' Default is 1e-3.
 #' @param verbose a logical value indicating whether to print messages
 #'  during the fitting process. Default is \code{FALSE}.
 survtrans_control <- function(
     eps = 1e-4, maxit = 1000, inner.eps = 1e-3, inner.maxit = 100,
-    verbose = FALSE) {
+    abstol = 1e-4, reltol = 1e-3, verbose = FALSE) {
   if (eps <= 0) stop("Invalid convergence criteria")
   if (maxit < 0) stop("Invalid value for iterations")
   if (inner.eps <= 0) stop("Invalid convergence criteria for inner loop")
   if (inner.maxit < 0) stop("Invalid value for iterations for inner loop")
+
+  if (abstol <= 0) stop("Invalid absolute tolerance")
+  if (reltol <= 0) stop("Invalid relative tolerance")
   if (!is.logical(verbose)) stop("Invalid value for verbose")
   list(
     eps = eps, maxit = as.integer(maxit), inner.eps = inner.eps,
-    inner.maxit = as.integer(inner.maxit), verbose = verbose
+    inner.maxit = as.integer(inner.maxit),
+    abstol = abstol, reltol = reltol, verbose = verbose
   )
 }
